90d5b237245e50d3574df01a1ebfffd15f61d67a
phtechin <phtechin@central.co.th>
2024-02-10 18:35:24 +0700

    fix: add long tag for server config

--- pkg/util/env/config.go
+++ pkg/util/env/config.go
@@ -6,11 +6,13 @@ import (
 
 var serverConfig ServerConfig
 
+// "env" use to read from .json
+// "long" use to read from .env
 type ServerConfig struct {
-	ProjectId   string `env:"PROJECT_ID"`
-	ServiceName string `env:"SERVICE_NAME"`
-	ServiceHost string `env:"SERVICE_HOST"`
-	ServicePort string `env:"SERVICE_PORT"`
+	ProjectId   string `long:"PROJECT_ID" env:"PROJECT_ID"`
+	ServiceName string `long:"SERVICE_NAME" env:"SERVICE_NAME"`
+	ServiceHost string `long:"SERVICE_HOST" env:"SERVICE_HOST"`
+	ServicePort string `long:"SERVICE_PORT" env:"SERVICE_PORT"`
 }
 
 func LoadConfig() {
