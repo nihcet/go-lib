a88696b8c9bb79e8dd1ea641bd6ca917f31907d1
phtechin <phtechin@central.co.th>
2024-02-10 18:32:17 +0700

    fix: upper server config cases

--- pkg/util/env/config.go
+++ pkg/util/env/config.go
@@ -7,10 +7,10 @@ import (
 var serverConfig ServerConfig
 
 type ServerConfig struct {
-	ProjectId   string `env:"project_id"`
-	ServiceName string `env:"service_name"`
-	ServiceHost string `env:"service_host"`
-	ServicePort string `env:"service_port"`
+	ProjectId   string `env:"PROJECT_ID"`
+	ServiceName string `env:"SERVICE_NAME"`
+	ServiceHost string `env:"SERVICE_HOST"`
+	ServicePort string `env:"SERVICE_PORT"`
 }
 
 func LoadConfig() {
